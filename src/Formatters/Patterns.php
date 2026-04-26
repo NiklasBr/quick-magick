fffb7f7639ebd54ff9c85c7b27d4115705d67554
Niklas Brunberg <n.brunberg@gmail.com>
2026-04-26 10:54:03 +0200

    Updated tests and Enum for patterns

--- src/Formatters/Patterns.php
+++ src/Formatters/Patterns.php
@@ -13,72 +13,12 @@ use NiklasBr\QuickMagick\Enums\Category;
 
 final class Patterns implements PseudoImageInterface
 {
-    /**
-     * @var string[]
-     */
-    public static array $validPatterns = [
-        'BRICKS',
-        'CHECKERBOARD',
-        'CIRCLES',
-        'CROSSHATCH',
-        'CROSSHATCH30',
-        'CROSSHATCH45',
-        'FISHSCALES',
-        'GRAY0',
-        'GRAY5',
-        'GRAY10',
-        'GRAY15',
-        'GRAY20',
-        'GRAY25',
-        'GRAY30',
-        'GRAY35',
-        'GRAY40',
-        'GRAY45',
-        'GRAY50',
-        'GRAY55',
-        'GRAY60',
-        'GRAY65',
-        'GRAY70',
-        'GRAY75',
-        'GRAY80',
-        'GRAY85',
-        'GRAY90',
-        'GRAY95',
-        'GRAY100',
-        'HEXAGONS',
-        'HORIZONTAL',
-        'HORIZONTAL2',
-        'HORIZONTAL3',
-        'HORIZONTALSAW',
-        'HS_BDIAGONAL',
-        'HS_CROSS',
-        'HS_DIAGCROSS',
-        'HS_FDIAGONAL',
-        'HS_HORIZONTAL',
-        'HS_VERTICAL',
-        'LEFT30',
-        'LEFT45',
-        'LEFTSHINGLE',
-        'OCTAGONS',
-        'RIGHT30',
-        'RIGHT45',
-        'RIGHTSHINGLE',
-        'SMALLFISHSCALES',
-        'VERTICAL',
-        'VERTICAL2',
-        'VERTICAL3',
-        'VERTICALBRICKS',
-        'VERTICALLEFTSHINGLE',
-        'VERTICALRIGHTSHINGLE',
-        'VERTICALSAW',
-    ];
-
     public static function format(Category $imageType, ?string $arg): string
     {
         $normalizedPattern = \strtoupper((string) $arg);
 
-        if (!\in_array($normalizedPattern, self::$validPatterns, true)) {
-            throw new \InvalidArgumentException('Unsupported pattern');
+        if (!\NiklasBr\QuickMagick\Enums\Patterns::isValid($normalizedPattern)) {
+            throw new \InvalidArgumentException("Invalid pattern: {$normalizedPattern}");
         }
 
         return "{$imageType->value}:{$normalizedPattern}";
